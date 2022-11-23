87aa4b6b4e94a71c4d385e7fded3cf28f24055ff
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2022-11-23 15:08:18 -0300

    Segundo exercício. Comentei o primeiro para no console mostrar só um resultado

--- fundamentos/secao-4-introducao-a-JavaScript/dia-1-primeiros-passos/exercicios-do-dia.js
+++ fundamentos/secao-4-introducao-a-JavaScript/dia-1-primeiros-passos/exercicios-do-dia.js
@@ -1,9 +1,20 @@
-const a = 35
-const b = 32
+const a = 38
+const b = 37
+const c = 51
 
-if (a > b) {
+/*if (a > b) {
     console.log(a)
 }
 else {
     console.log(b)
+}*/
+
+if (a > b && a > c) {
+    console.log(a)
+}
+else if (b > a && b > c) {
+    console.log(b)
+}
+else {
+    console.log(c)
 }
