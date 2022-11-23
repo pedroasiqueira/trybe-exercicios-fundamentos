a57feb40cfed380c57c8f877d8ac6690db1e50e9
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2022-11-23 10:52:13 -0300

    Finalizada parte de condições If/Else

--- fundamentos/secao-4-introducao-a-JavaScript/dia-1-primeiros-passos/scripts.js
+++ fundamentos/secao-4-introducao-a-JavaScript/dia-1-primeiros-passos/scripts.js
@@ -15,3 +15,36 @@ let color = null; //redefinição
 //console.log(typeof score)
 
 //console.log(2 ** 3)
+
+
+//IF / ELSE
+
+//Exemplo da aula:
+let trybe = 16.42;
+
+if (trybe >= 14 && trybe < 14.40) {
+    console.log("Esquenta");
+}
+else if (trybe >= 16.30 && trybe < 17.50) {
+    console.log("Aula ao vivo")
+}
+else if (trybe >= 19.40 && trybe <= 20) {
+    console.log("Fechamento")
+}
+else {
+    console.log("Fora dos momentos síncronos")
+}
+
+const aprovado = 80;
+const notaMinima = 60;
+const pessoa = 79;
+
+if (pessoa >= aprovado) {
+    console.log("Parabéns, você foi aprovada(o)")
+}
+else if (pessoa < aprovado && pessoa > notaMinima) {
+    console.log("Você está na nossa lista de espera")
+}
+else {
+    console.log("Você foi reprovado")
+}
