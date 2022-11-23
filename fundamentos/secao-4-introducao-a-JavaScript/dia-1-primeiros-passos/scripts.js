221ce66863e708dd899cb78f3f8d3f369e4ff8bc
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2022-11-23 10:30:53 -0300

    tipos primirivos, tipagem dinâmica e operações aritméticas

--- fundamentos/secao-4-introducao-a-JavaScript/dia-1-primeiros-passos/scripts.js
+++ fundamentos/secao-4-introducao-a-JavaScript/dia-1-primeiros-passos/scripts.js
@@ -2,4 +2,16 @@ const myNAme = "Pedro"
 const birthCity = "Natal"
 let birthYear = 1995
 
-console.log(myNAme, birthCity, birthYear)
+//console.log(myNAme, birthCity, birthYear)
+
+//Tipos primitivos, Tipagem dinâmica e Operações aritméticas
+
+let movie = "Avengers"; //string literal
+let score = 10.89 //number literal
+let isValid = true; //boolean
+let namee; //undefined
+let color = null; //redefinição
+
+//console.log(typeof score)
+
+//console.log(2 ** 3)
