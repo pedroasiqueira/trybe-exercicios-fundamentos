91e87c6f09de11ee1cf876f3031a2da0f9ff304f
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2022-11-23 15:55:56 -0300

    último exercicio de if/else. Penei aqui viu. Penei mas terminei

--- fundamentos/secao-4-introducao-a-JavaScript/dia-1-primeiros-passos/exercicios-do-dia.js
+++ fundamentos/secao-4-introducao-a-JavaScript/dia-1-primeiros-passos/exercicios-do-dia.js
@@ -1,6 +1,6 @@
-const a = 0
-const b = 37
-const c = 51
+const a = 0;
+const b = 37;
+const c = 51;
 
 /*if (a > b) {
     console.log(a)
@@ -17,7 +17,7 @@ else if (b > a && b > c) {
 }
 else {
     console.log(c)
-}*/
+}
 
 if (a > 0) {
     console.log("positive")
@@ -27,4 +27,21 @@ else if (a < 0) {
 }
 else if (a === 0) {
     console.log("zero")
+}*/
+
+const primeiroAngulo = 180;
+const segundoAngulo = -1;
+const terceiroAngulo = -1;
+
+if (primeiroAngulo + segundoAngulo + terceiroAngulo === 180 && primeiroAngulo >= 0 && segundoAngulo >= 0 && terceiroAngulo >= 0) {
+    console.log("true")
+}
+else if (primeiroAngulo + segundoAngulo + terceiroAngulo !== 180 && primeiroAngulo >= 0 && segundoAngulo >= 0 && terceiroAngulo >= 0) {
+    console.log("false")
+}
+/*else if (primeiroAngulo <= -1 || segundoAngulo <= -1 || terceiroAngulo <= -1) {
+    console.log("erro")
+}*/
+else {
+    console.log("erro")
 }
