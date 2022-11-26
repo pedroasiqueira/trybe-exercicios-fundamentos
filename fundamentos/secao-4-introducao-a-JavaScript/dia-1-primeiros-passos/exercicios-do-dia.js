1686355d9f2bb048648dd1ab744ca265c7216249
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2022-11-26 09:08:51 -0300

    Fear: adicionando exercicio de if/else. Notas em porcentagem.

--- fundamentos/secao-4-introducao-a-JavaScript/dia-1-primeiros-passos/exercicios-do-dia.js
+++ fundamentos/secao-4-introducao-a-JavaScript/dia-1-primeiros-passos/exercicios-do-dia.js
@@ -57,3 +57,27 @@ switch(peça) {
         break;
     default: console.log("peça não encontrada no sistema")
 }
+
+let nota = 90;
+
+if (nota >= 90 && nota <= 100) {
+    console.log("A")
+}
+else if (nota >= 80 && nota < 90) {
+    console.log("B")
+}
+else if (nota >= 70 && nota < 80) {
+    console.log("C")
+}
+else if (nota >= 60 && nota < 70) {
+    console.log("D")
+}
+else if (nota >= 50 && nota < 60) {
+    console.log("E")
+}
+else if (nota < 50 && nota === 0) {
+    console.log("F")
+}
+else if (nota < 0 || nota > 100) {
+    console.log("erro")
+}
