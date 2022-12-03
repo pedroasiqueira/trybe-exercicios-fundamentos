2ccf2f54973f0f51ed7caa28aa94caf9048032a5
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2022-12-03 09:58:43 -0300

    criação de exemplode Array

---
+++ fundamentos/secao-4-introducao-a-JavaScript/dia-2-array-e-loop-for/array-and-loops-aula-ao-vivo.js
@@ -0,0 +1,5 @@
+const lotteryNumbers = [4, 22, 23, 26, 34, 46]
+
+for(let i = 0; i < lotteryNumbers.length; i += 1) {
+    console.log(lotteryNumbers[i])
+}
--- fundamentos/secao-4-introducao-a-JavaScript/dia-2-array-e-loop-for/while.js
+++ fundamentos/secao-4-introducao-a-JavaScript/dia-2-array-e-loop-for/while.js
@@ -12,12 +12,3 @@ if (result === 0) {
 } else {
   console.log(result);
 }
-
-
-let number = [];
-
-for (let index = 1; index <= 25; index += 1) {
-  number.push(index);
-}
-
-console.log(number);
