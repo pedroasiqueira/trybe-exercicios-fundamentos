d9b87e4caacd0f0c72c7f20214597a6740cffdb2
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-02-01 10:20:03 -0300

    arquivo forEach.js

---
+++ 1- Introdução a HOFs/forEach.js
@@ -0,0 +1,32 @@
+// Ex 1
+// const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+// const [zero, primeiro, segundo, terceiro] = arrayOfValues
+
+// arrayOfValues.forEach((element, index) => {
+//   console.log(`O elemento atual é: ${element} e possui o index: ${index}`);
+// });
+
+
+// const pessoasAprovadas = ['Ana Beatriz', 'Caio Nunes', 'Afonso Ribeiro', 'Leonardo Lins']; // lista de nomes
+
+// pessoasAprovadas.forEach((element, index) => {
+//     pessoasAprovadas[index] = pessoasAprovadas[index].toUpperCase();
+// })
+// console.log(pessoasAprovadas)
+
+
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// numbers.forEach((number) => {
+//     console.log(number * 2);
+// })
+
+
+const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+  ];
+  
+  emailListInData.forEach((element) => console.log(`O email ${element} está cadastrado em nosso banco de dados!`))
