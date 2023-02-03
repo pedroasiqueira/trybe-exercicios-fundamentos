89d627e7a31cd3a213757fac5a9b88896742efb0
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-02-03 17:28:16 -0300

    exercicios bonus

---
+++ fundamentos/secao-4-introducao-a-JavaScript/dia-5-arrow-function/exercicios/exercicios-bonus.js
@@ -0,0 +1,46 @@
+// MINHA RESOLUÇÃO:
+const substituaX = (nome) => {
+    const frase = `Tryber x aqui`;
+    return frase.replace('x', `${nome}`);
+}
+
+
+//RESOLUÇÃO DA TRYBE:
+// const substituaX = (nome) => {
+//     const frase = 'Tryber x aqui!';
+//     const fraseArray = frase.split(' ');
+//     for (let index = 0; index < fraseArray.length; index += 1) {
+//       if (fraseArray[index] === 'x') {
+//         fraseArray[index] = nome;
+//       }
+//     }  
+//     return fraseArray.join(' ');
+//   };
+  
+//   console.log(substituaX('pedro'));
+
+
+
+// const minhasSkills = (param) => {
+//     const skills = ['HTML', 'CSS', 'JavaScript'];
+//     let concatenacao = `${param}
+//     Minhas três principais habilidades são:
+//         ${skills[0]}
+//         ${skills[1]}
+//         ${skills[2]}`
+//     return concatenacao
+//   };
+
+
+  const minhasSkills = (param) => {
+    const skills = ['HTML', 'CSS', 'JavaScript'];
+    let concatenacao = `${param}
+    Minhas três principais habilidades são:`
+    for(let index = 0; index < skills.length; index += 1) {
+        concatenacao = `${concatenacao}
+        -${skills[index]}`
+    }
+    return concatenacao
+  };
+
+  console.log(minhasSkills(substituaX('Pedro')))
