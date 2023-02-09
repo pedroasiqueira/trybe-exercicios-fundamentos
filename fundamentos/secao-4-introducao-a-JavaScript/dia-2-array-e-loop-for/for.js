8c1f3f502cafa9a1c0fdbc5fe3d40f3cdc1aee09
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-02-09 12:29:02 -0300

    testando

--- fundamentos/secao-4-introducao-a-JavaScript/dia-2-array-e-loop-for/for.js
+++ fundamentos/secao-4-introducao-a-JavaScript/dia-2-array-e-loop-for/for.js
@@ -1,17 +1,27 @@
-let numero = 7;
+// let numero = 7;
 
-for (let i = 1; i <= 9; i += 1) {
-    console.log(numero * i)
+// for (let i = 1; i <= 9; i += 1) {
+//     console.log(numero * i)
+// }
+
+// let nomes = ['Joana', 'Maria', 'Lucas']
+
+// for (let i = 0; i < nomes.length; i += 1) {
+//     console.log('Seja bem vindo(a) ' + nomes[i])
+// }
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let i = 0; i < groceryList.length; i += 1) {
+//     console.log(groceryList[i])
+// }
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index]
 }
 
-let nomes = ['Joana', 'Maria', 'Lucas']
-
-for (let i = 0; i < nomes.length; i += 1) {
-    console.log('Seja bem vindo(a) ' + nomes[i])
-}
-
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
-
-for (let i = 0; i < groceryList.length; i += 1) {
-    console.log(groceryList[i])
-}
+console.log(soma)
+console.log('soma')
