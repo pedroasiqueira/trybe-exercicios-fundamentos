9061a6953412b1c92ccc2dcd8eed2926b2111115
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-03-17 11:27:32 -0300

    setTimeout.js => modificando o número 1000 porque o eslint tava dando problema. Coloquei esse número dentro de uma variável e não reclamou mais

--- 1-introducaoFront-JSAssicrono/Dia-2-Js-Assíncrono-Promisses-e-Fetch/setTimeout.js
+++ 1-introducaoFront-JSAssicrono/Dia-2-Js-Assíncrono-Promisses-e-Fetch/setTimeout.js
@@ -1,2 +1,13 @@
 const firstNumber = 9;
 const secondNumber = 3;
+
+let thirdNumber = firstNumber + secondNumber;
+console.log(thirdNumber);
+
+const time = 1000;
+setTimeout(() => {
+  thirdNumber = firstNumber - secondNumber;
+  console.log(thirdNumber);
+}, time);
+
+console.log(thirdNumber);
