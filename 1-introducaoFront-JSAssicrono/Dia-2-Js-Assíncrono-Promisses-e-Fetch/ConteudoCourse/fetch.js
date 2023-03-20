b85289e996ce283c1b674b0adf0c1289ddfc672a
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-03-20 09:43:55 -0300

    finalizando fetch

--- 1-introducaoFront-JSAssicrono/Dia-2-Js-Assíncrono-Promisses-e-Fetch/ConteudoCourse/fetch.js
+++ 1-introducaoFront-JSAssicrono/Dia-2-Js-Assíncrono-Promisses-e-Fetch/ConteudoCourse/fetch.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 
-fetch('https://dummyjson.com/quotes/random')
+fetch('https://invalid.dummyjson.com/quotes/random')
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => console.log(data.quote))
+  .catch((error) => console.log(`Erro na requisição: ${error.message}`));
