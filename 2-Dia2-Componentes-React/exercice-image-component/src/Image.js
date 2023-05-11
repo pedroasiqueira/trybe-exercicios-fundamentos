bb3526b48f00eb9757a9cd15cf2aee591d0af21e
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-05-11 12:53:45 -0300

    adc

--- 2-Dia2-Componentes-React/exercice-image-component/src/Image.js
+++ 2-Dia2-Componentes-React/exercice-image-component/src/Image.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css'
 
 class Image extends React.Component {
   render() {
     const { source, alternativeText } = this.props;
-    return <img className="gato" src={ source } alt={ alternativeText } />;
+    return <img src={ source } alt={ alternativeText } />;
   }
 }
 
